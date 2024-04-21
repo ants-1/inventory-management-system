5adbc5ed64754fb9f75e775ca9611277382bdf31
ants-1 <anthonysalisbury98@outlook.com>
2024-04-21 18:08:11 +0100

    Add templates in base and setup urls for each template

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,18 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="base_home")
+    path('', views.landing_page, name='base_landing_page'),
+    path('sign_up', views.sign_up, name='base_sign_up'),
+    path('login', views.login, name='base_login'),
+    path('edit_profile', views.edit_profile, name='base_edit_profile'), # Form
+    path('equipment_details', views.equipment_details),
+    path('user_profile', views.user_profile, name='base_user_profile'),
+    path('user_alerts', views.user_alerts, name='base_user_alerts'),
+    path('bookings', views.bookings, name='base_bookings'),
+    path('admin_profile', views.admin_profile, name='base_admin_profile'),
+    path('add_equipment', views.add_equipment, name='base_add_equipment'),
+    path('edit_equipment', views.edit_equipement, name='base_edit_equipment'),
+    path('edit_users', views.edit_users, name='base_edit_users'),
+    path('reports', views.reports, name='base_reports'),
+    path('approvals', views.approvals, name='base_approvals')
 ]
--- base/views.py
+++ base/views.py
@@ -2,8 +2,51 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'base/home.html')
+def landing_page(request):
+    return render(request, 'base/landing-page.html')
 
 def navbar(request):
     return render(request, 'navbar.html')
+
+def sign_up(request):
+    return render(request, 'base/sign-up.html')
+
+def login(request):
+    return render(request, 'base/login.html')
+
+def edit_profile(request):
+    return render(request, 'base/edit-profile.html')
+
+def equipment_details(request):
+    return render(request, 'base/equipment-details.html')
+
+# User pages
+
+def user_profile(request):
+    return render(request, 'base/user-profile.html')
+
+def user_alerts(request):
+    return render(request, 'base/user-alerts.html')
+
+def bookings(request):
+    return render(request, 'base/bookings.html')
+
+# Admin pages
+
+def admin_profile(request):
+    return render(request, 'base/admin-profile.html')
+
+def add_equipment(request):
+    return render(request, 'base/add-equipment.html')
+
+def edit_equipement(request):
+    return render(request, 'base/edit-equipment.html')
+
+def edit_users(request):
+    return render(request, 'base/edit-users.html')
+
+def reports(request):
+    return render(request, 'base/reports.html')
+
+def approvals(request):
+    return render(request, 'base/approvals.html')
