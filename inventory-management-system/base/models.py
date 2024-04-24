c7156fa96640997995344f9124d025b38e719178
alishasc <124906692+alishasc@users.noreply.github.com>
2024-04-24 16:04:07 +0100

    Added migrations

---
+++ inventory-management-system/base/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    role_description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+class Admin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    approval_status = models.ManyToManyField(User, related_name='managed_approval_status', blank=True)
+    users_manage = models.ManyToManyField(User, related_name='managed_users', blank=True)
+    equipment_manage = models.ManyToManyField('Equipment', related_name='managed_equipment', blank=True)
+    booking_approval = models.ManyToManyField('Booking', related_name='confirmed_by_admin', blank=True)
+    reservation_approval = models.ManyToManyField('Reservation', related_name='approved_by_admin', blank=True)
+ 
+    def __str__(self):
+        return self.name
+
+class Equipment(models.Model):
+    category = models.CharField(max_length=254)
+    description = models.CharField(max_length=254)
+    equipment_count = models.IntegerField(default=0)
+    equipment_type = models.CharField(max_length=254)
+    borrow_date = models.DateField()
+    return_date = models.DateField()
+    on_site_only = models.BooleanField()
+    availability = models.ManyToManyField(User, related_name='reserved_equipment', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    booking_duration = models.DurationField() # Maybe change to DateField()
+    approval_status = models.BooleanField(default=False)
+    cancellation = models.BooleanField(default=False)
+    rejection_reason = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    booking_date = models.DateField()
+    return_date = models.DateField()
+
+    def __str__(self):
+        return self.name
