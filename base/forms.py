75caaeb7e343d8bbd0626d808556ca06703fc4f8
ants-1 <anthonysalisbury98@outlook.com>
2024-04-28 09:55:14 +0100

    Updated views to handle form submission for adding equipment

---
+++ base/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import *
+
+
+class AddEquipmentForm(forms.ModelForm):
+
+    class Meta:
+        model = Equipment
+        fields = [
+            "name",
+            "type",
+            "description",
+            "quantity",
+            "borrow_date",
+            "return_date",
+            "audit_date",
+            "status",
+            "serial_number",
+            "comments",
+            "location",
+            "img_url",
+        ]
