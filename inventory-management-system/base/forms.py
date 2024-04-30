f338bedd14a6a880a5ad36dd7e56ec3f57c21d1a
alishasc <124906692+alishasc@users.noreply.github.com>
2024-04-30 12:45:24 +0100

    Login/logout functionality
    Can only login using superuser details atm. Nav bar changes depending if logged in or not

---
+++ inventory-management-system/base/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+
+class AddEquipmentForm(forms.ModelForm):
+    class Meta:
+        model = Equipment
+        fields = "__all__"
+
+class EditEquipmentForm(forms.ModelForm):    
+    class Meta:
+        model = Equipment
+        fields = "__all__"
+
+class AddUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = "__all__"
